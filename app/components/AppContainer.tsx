20de7a892fc812b120e0a9b02e6527756f8c848b
Shubham Maurya <65014926+Shubham996633@users.noreply.github.com>
2023-09-05 15:23:45 +0530

    update

--- app/components/AppContainer.tsx
+++ app/components/AppContainer.tsx
@@ -10,15 +10,23 @@ import {
 } from "@react-three/drei";
 import Ground from "./Ground";
 import Rings from "./Rings";
+import { Boxes } from "./Boxes";
+import { Bloom,ChromaticAberration, EffectComposer  } from "@react-three/postprocessing";
+import {BlendFunction} from 'postprocessing'
+import { FloatingGrid } from "./FloatingGrid";
 
 function CarShow() {
   return (
     <>
-      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+    <OrbitControls 
+        target={[0, 0.35, 0]}
+        maxPolarAngle={1.45}
+      />
 
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
       <color args={[0, 0, 0]} attach="background" />
+
       <CubeCamera resolution={256} frames={Infinity}>
         {(texture) => (
           <>
@@ -28,7 +36,6 @@ function CarShow() {
         )}
       </CubeCamera>
 
-      <Rings />
       <spotLight
         color={[1, 0.25, 0.7]}
         intensity={1.5}
@@ -48,8 +55,26 @@ function CarShow() {
         shadow-bias={-0.0001}
       />
       <Ground />
-
-      <Ground />
+      <Boxes />
+      <Rings />
+      <FloatingGrid/>
+      <EffectComposer>
+        {/* <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
+        <Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={1.3} // The bloom intensity.
+          width={300} // render width
+          height={300} // render height
+          kernelSize={5} // blur kernel size
+          luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+        />
+        <ChromaticAberration
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          //@ts-ignore
+          offset={[0.0005, 0.0012]} // color offset
+        />
+      </EffectComposer>
     </>
   );
 }
